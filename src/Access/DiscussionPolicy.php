EricYChu/flarum-ext-tags

Make "own" permissions depend on the user's ability to reply
See https://github.com/flarum/core/pull/1513

src/Access/DiscussionPolicy.php
@@ -120,7 +120,7 @@ class DiscussionPolicy extends AbstractPolicy
      */
     public function tag(User $actor, Discussion $discussion)
     {
-        if ($discussion->start_user_id == $actor->id) {
+        if ($discussion->start_user_id == $actor->id && $actor->can('reply', $discussion)) {
             $allowEditTags = $this->settings->get('allow_tag_change');
 
             if ($allowEditTags === '-1'

flarum/tags

src/Access/DiscussionPolicy.php
@@ -120,7 +120,7 @@ class DiscussionPolicy extends AbstractPolicy
      */
     public function tag(User $actor, Discussion $discussion)
     {
-        if ($discussion->user_id == $actor->id) {
+        if ($discussion->user_id == $actor->id && $actor->can('reply', $discussion)) {
             $allowEditTags = $this->settings->get('allow_tag_change');
 
             if ($allowEditTags === '-1'