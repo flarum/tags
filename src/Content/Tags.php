c889ad88377b599b88060e644df281aadfff3ecc
David Sevilla Martin <dsevilla192@icloud.com>
2019-08-10 17:20:35 -0400

    Add canonical URL to tags page
    Fixes flarum/core#1134

--- src/Content/Tags.php
+++ src/Content/Tags.php
@@ -13,6 +13,8 @@ namespace Flarum\Tags\Content;
 
 use Flarum\Api\Client;
 use Flarum\Frontend\Document;
+use Flarum\Http\UrlGenerator;
+use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\Tags\TagRepository;
 use Illuminate\Contracts\View\Factory;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -35,21 +37,38 @@ class Tags
     protected $tags;
 
     /**
+     * @var SettingsRepositoryInterface
+     */
+    protected $settings;
+
+    /**
+     * @var UrlGenerator
+     */
+    protected $url;
+
+    /**
      * @param Client $api
      * @param Factory $view
+     * @param TagRepository $tags
+     * @param SettingsRepositoryInterface $settings
+     * @param UrlGenerator $url
      */
-    public function __construct(Client $api, Factory $view, TagRepository $tags)
+    public function __construct(Client $api, Factory $view, TagRepository $tags, SettingsRepositoryInterface $settings, UrlGenerator $url)
     {
         $this->api = $api;
         $this->view = $view;
         $this->tags = $tags;
+        $this->settings = $settings;
+        $this->url = $url;
     }
 
     public function __invoke(Document $document, Request $request)
     {
         $tags = collect($document->payload['resources'])->where('type', 'tags');
+        $defaultRoute = $this->settings->get('default_route');
 
         $document->content = $this->view->make('tags::frontend.content.tags', compact('tags'));
+        $document->canonicalUrl = $defaultRoute === '/tags' ? $this->url->to('forum')->base() : $request->getUri()->withQuery('');
 
         return $document;
     }
