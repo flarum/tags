6393e623f1e1b5e02465958c8e31d98c3a77f5c1
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-08 02:22:24 -0500

    Add some more tests

--- tests/integration/api/CreateTest.php
+++ tests/integration/api/CreateTest.php
@@ -9,8 +9,10 @@
 
 namespace Flarum\Tags\Tests\integration\api\users;
 
+use Flarum\Tags\Tag;
 use Flarum\Testing\integration\RetrievesAuthorizedUsers;
 use Flarum\Testing\integration\TestCase;
+use Illuminate\Support\Arr;
 
 class CreateTest extends TestCase
 {
@@ -35,7 +37,7 @@ class CreateTest extends TestCase
     /**
      * @test
      */
-    public function mormal_user_cant_create_tag()
+    public function normal_user_cant_create_tag()
     {
         $response = $this->send(
             $this->request(
@@ -46,5 +48,60 @@ class CreateTest extends TestCase
         );
 
         $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function admin_cannot_create_tag_without_data()
+    {
+        $response = $this->send(
+            $this->request('POST', '/api/tags', [
+                'authenticatedAs' => 1,
+                'json' => [],
+            ])
+        );
+
+        $this->assertEquals(422, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function admin_can_create_tag()
+    {
+        $response = $this->send(
+            $this->request('POST', '/api/tags', [
+                'authenticatedAs' => 1,
+                'json' => [
+                    'data' => [
+                        'attributes' => [
+                            'name' => 'Dev Blog',
+                            'slug' => 'dev-blog',
+                            'description' => 'Follow Flarum development!',
+                            'color' => '#123456'
+                        ],
+                    ],
+                ],
+            ])
+        );
+
+        $this->assertEquals(201, $response->getStatusCode());
+
+        // Verify API response body
+        $data = json_decode($response->getBody(), true);
+        $this->assertEquals('Dev Blog', Arr::get($data, 'data.attributes.name'));
+        $this->assertEquals('dev-blog', Arr::get($data, 'data.attributes.slug'));
+        $this->assertEquals('Follow Flarum development!', Arr::get($data, 'data.attributes.description'));
+        $this->assertEquals('#123456', Arr::get($data, 'data.attributes.color'));
+        $this->assertNull(Arr::get($data, 'data.attributes.icon'));
+
+        // Verify database entry
+        $tag = Tag::all()->last();
+        $this->assertEquals('Dev Blog', $tag->name);
+        $this->assertEquals('dev-blog', $tag->slug);
+        $this->assertEquals('Follow Flarum development!', $tag->description);
+        $this->assertEquals('#123456', $tag->color);
+        $this->assertNull($tag->icon);
     }
 }
