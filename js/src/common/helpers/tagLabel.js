dfa9987bbfdd8e43d440bd4139836c96e6cae9ff
David Wheatley <hi@davwheat.dev>
2021-02-01 01:43:31 +0000

    Fix formatting

--- js/src/common/helpers/tagLabel.js
+++ js/src/common/helpers/tagLabel.js
@@ -1,38 +1,37 @@
-import extract from "flarum/utils/extract";
-import Link from "flarum/components/Link";
-import tagIcon from "./tagIcon";
+import extract from 'flarum/utils/extract';
+import Link from 'flarum/components/Link';
+import tagIcon from './tagIcon';
 
 export default function tagLabel(tag, attrs = {}) {
   attrs.style = attrs.style || {};
-  attrs.className = "TagLabel " + (attrs.className || "");
+  attrs.className = 'TagLabel ' + (attrs.className || '');
 
-  const link = extract(attrs, "link");
-  const tagText = tag
-    ? tag.name()
-    : app.translator.trans("flarum-tags.lib.deleted_tag_text");
+  const link = extract(attrs, 'link');
+  const tagText = tag ? tag.name() : app.translator.trans('flarum-tags.lib.deleted_tag_text');
 
   if (tag) {
     const color = tag.color();
     if (color) {
       attrs.style.backgroundColor = attrs.style.color = color;
-      attrs.className += " colored";
+      attrs.className += ' colored';
     }
 
     if (link) {
-      attrs.title = tag.description() || "";
-      attrs.href = app.route("tag", { tags: tag.slug() });
+      attrs.title = tag.description() || '';
+      attrs.href = app.route('tag', {tags: tag.slug()});
     }
 
     attrs["data-is-child"] = tag.data.attributes.isChild ? "true" : "false";
+    console.log(tag);
   } else {
-    attrs.className += " untagged";
+    attrs.className += ' untagged';
   }
 
-  return m(
-    link ? Link : "span",
-    attrs,
-    <span className="TagLabel-text">
-      {tag && tag.icon() && tagIcon(tag, {}, { useColor: false })} {tagText}
-    </span>
+  return (
+    m((link ? Link : 'span'), attrs,
+      <span className="TagLabel-text">
+        {tag && tag.icon() && tagIcon(tag, {}, {useColor: false})} {tagText}
+      </span>
+    )
   );
 }
