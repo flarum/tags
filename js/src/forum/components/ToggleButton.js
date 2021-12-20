518e2f399dac1ac57687622793eddb1badd21832
SychO9 <sychocouldy@gmail.com>
2021-12-20 16:56:30 +0100

    chore: Add todo

--- js/src/forum/components/ToggleButton.js
+++ js/src/forum/components/ToggleButton.js
@@ -2,6 +2,9 @@ import Component from 'flarum/common/Component';
 import Button from 'flarum/common/components/Button';
 import classList from 'flarum/common/utils/classList';
 
+/**
+ * @TODO move to core
+ */
 export default class ToggleButton extends Component {
   view(vnode) {
     const { className, isToggled, ...attrs } = this.attrs;
