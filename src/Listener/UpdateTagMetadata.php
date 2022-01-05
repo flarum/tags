727431ae56716865908e6cbf1512fb5d0c2ce8b1
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-01-04 19:23:00 -0500

    If a discussion doesn't exist, it can't be the last posted discussion
    This fixes some rare errors where deleting the only post in the last posted discussion of a tag triggers a DB integrity exception.

--- src/Listener/UpdateTagMetadata.php
+++ src/Listener/UpdateTagMetadata.php
@@ -145,7 +145,7 @@ class UpdateTagMetadata
 
             // If this is a new / restored discussion, it isn't private, it isn't null,
             // and it's more recent than what we have now, set it as last posted discussion.
-            if ($delta >= 0 && ! $discussion->is_private && $discussion->hidden_at == null && ($discussion->last_posted_at >= $tag->last_posted_at)) {
+            if ($delta >= 0 && ! $discussion->is_private && $discussion->hidden_at == null && ($discussion->last_posted_at >= $tag->last_posted_at) && $discussion->exists) {
                 $tag->setLastPostedDiscussion($discussion);
             } elseif ($discussion->id == $tag->last_posted_discussion_id) {
                 // This is to persist refreshLastPost above. It is here instead of there so that
